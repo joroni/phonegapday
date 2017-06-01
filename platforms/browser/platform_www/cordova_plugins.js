4dd85d49bc3f604f479f769c7eb4a6d3bb07b479
kophonegap <niconi@coca-cola.com>
2017-06-02 03:00:43 +0800

    files

---
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.wikitude.phonegap.WikitudePlugin/www/WikitudePlugin.js",
+        "id": "com.wikitude.phonegap.WikitudePlugin.WikitudePlugin",
+        "pluginId": "com.wikitude.phonegap.WikitudePlugin",
+        "clobbers": [
+            "WikitudePlugin"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.1",
+    "com.wikitude.phonegap.WikitudePlugin": "6.1.0"
+}
+// BOTTOM OF METADATA
+});
